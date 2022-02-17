2bc0e092bba5792b8347d58b14ad5fc3f23f96f3
salmonvanus <stevanussalmon@gmail.com>
2022-02-17 17:15:01 +0800

    comit ketiga

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -14,7 +14,7 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Orbitron" type="text/css" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-	<title>hello</title>
+	<title>salmonvanus</title>
 
 	<style>
 		* {
